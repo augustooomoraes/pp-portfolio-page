ba9f1f99e39d716cfed4386c91165dda8b449579
Augusto Moraes <cdm.augusto@gmail.com>
2023-12-15 06:40:33 -0300

    Introduction text updated. Checking now og issues.

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
             Chamo-me Augusto, moro em Caxias do Sul – RS e estudo <span className="font-extrabold dark:font-semibold">programação e desenvolvimento web</span> pela internet.
           </p>
           <p className="home-word-spacing dark:tracking-[0.4rem] dark:font-light">
-            Procuro empregos onde poderia mais eficientemente <span className="font-extrabold dark:font-semibold">estender e aprofundar meus conhecimentos e habilidades</span> na área de desenvolvimento e programação em geral, com profissionais mais experientes e qualificados. Estou <span className="font-extrabold dark:font-semibold">aberto a desafios</span> e interessado em aprender o que for necessário para progredir profissionalmente e me inserir nesta área de atuação.
+            Procuro empregos onde poderia <span className="font-extrabold dark:font-semibold">estender e aprofundar meus conhecimentos e habilidades</span> na área de desenvolvimento e programação em geral, com profissionais mais experientes e qualificados. Estou <span className="font-extrabold dark:font-semibold">aberto a desafios</span> e interessado em aprender o que for necessário para progredir profissionalmente e me inserir nesta área de atuação.
           </p>
         </section>
         <Image width={280} height={280} className="rounded-full shadow-[15px_15px_15px_0_rgba(0,0,0,0.2)]" src={"/me0.jpg"} alt="" />
